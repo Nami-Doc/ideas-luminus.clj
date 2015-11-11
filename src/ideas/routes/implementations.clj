97de76e72e754e068a3a8a846d57d05736a4a099
vendethiel <vendethiel@hotmail.fr>
2015-11-11 21:22:10 +0100

    add some more stuff wrt implems
    still a big WIP

--- src/ideas/routes/implementations.clj
+++ src/ideas/routes/implementations.clj
@@ -1,19 +1,34 @@
 (ns ideas.routes.implementations
   (:use compojure.core)
-  (:require [ideas.views.layout :as layout]
+  (:require [noir.response :as resp]
+            [noir.validation :as vali]
+            [ideas.views.layout :as layout]
             [ideas.models.db :as db]
             [ideas.util :refer [parse-int]]))
 
-(defn add-page [idea-id]
-  (if-let [idea (db/get-idea (parse-int idea-id))]
-    (layout/render
-      "implementations/add.html"
-      {:idea idea})))
+(defn- valid? [my fields]
+  )
+
+(defn add-page [idea]
+  (layout/render
+    "implementations/add.html"
+    {:idea idea}))
+
+(defn save-page [idea fields]
+  (if (valid? fields)
+    (do
+      (save fields)
+      (resp/redirect (str "ideas/" (:id idea))))
+    (do
+      )))
 
 (defn crud-routes [idea-id]
-  (routes
-    ;; @XXX I probably don't need a "list"
-    (GET "/add" [] (add-page idea-id))))
+  (if-let [idea (db/get-idea (parse-int idea-id))]
+    (routes
+      (GET "/add" []
+        (is-auth! #(add-page idea)))
+      (POST "/" [my fields]
+        (is-auth! #(save-page my fields))))))
 
 (defroutes implementations-routes
   (context "/ideas/:idea-id/implementations" [idea-id]
