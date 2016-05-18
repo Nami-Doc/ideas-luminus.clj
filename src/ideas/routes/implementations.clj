92e12c52c85297c0367c42c717c9957c1bb51ef6
vendethiel <vendethiel@hotmail.fr>
2016-05-18 17:04:26 +0200

    Tiny refactorings for implementations

--- src/ideas/routes/implementations.clj
+++ src/ideas/routes/implementations.clj
@@ -6,25 +6,41 @@
              [request :refer [is-auth!]]]
             [ideas.util :refer [parse-int]]
             [ideas.views.layout :as layout]
-            [noir.session :as session]))
+            [noir.session :as session]
+            [noir.validation :as vali]))
 
 (def list-page (crud-for-list "implementation"))
+(defn- params
+  [idea repo demo comment]
+  {:idea idea :repo repo :demo demo :comment comment})
+
 (defn add-page
   ([idea] (add-page idea "" ""))
-  ([idea name url] (layout/render "implementations/add.html"
-                                  {:idea idea :name name :url url})))
+  ([idea repo demo comment]
+   (layout/render "implementations/add.html"
+                  (params idea repo demo comment))))
 
-(defn- valid? [name url]
-  true)
+(defn- valid? [repo demo comment]
+  ;; TODO assert that the repo is an URL
+  (vali/rule (vali/has-value? repo)
+             [:repo"you must specify a repository url"])
+  ; XXX is a demo required?
+  ;; (vali/rule (vali/has-value? demo)
+  ;;            [:description "you must specify a demo url"])
+  ; XXX is a comment required?
+  ;; (vali/rule (vali/min-length? description 5)
+  ;;            [:description "your description must be at least 5 characters"])
+  (not (vali/errors? repo demo comment)))
 
-(defn save-page [idea name url]
-  (if (valid? name url)
-    (do
-      (db/create-implementation {:name name :url url :idea_id (:id idea)})
-      (list-page))
+(defn save-page [idea repo demo comment]
+  (if (valid? repo demo comment)
     (do
       (session/flash-put! :error "Invalid implementation")
-      (add-page name url "Invalid implementation"))))
+      (add-page name url "Invalid implementation"))
+    (do
+      ;; TODO can use `params` here? or is `idea` a no-go for the ORM?
+      (db/create-implementation {:name name :url url :idea_id (:id idea)})
+      (list-page))))
 
 (defn crud-routes [idea-id]
   (if-let [idea (db/get-idea (parse-int idea-id))]
